d9f7573a5a7af79bf634d48d4e4383d42ae1ae84
sauriiiin <saur1n@users.noreply.github.com>
2019-10-22 14:49:04 -0400

    update

--- connSQL.m
+++ connSQL.m
@@ -6,12 +6,12 @@
 %   dr.saurin.parikh@gmail.com
 
 %%
-    function conn = connSQL(db, username, pwd)
+    function conn = connSQL(sql_info)
 
         url = sprintf(['jdbc:mysql://paris.csb.pitt.edu:3306/%s?',...
             'useUnicode=true&useJDBCCompliantTimezoneShift=true&',...
-            'useLegacyDatetimeCode=false&serverTimezone=UTC'],db);
-        conn = database('', username, pwd,'com.mysql.jdbc.Driver', url);
+            'useLegacyDatetimeCode=false&serverTimezone=UTC'],sql_info{3});
+        conn = database('', sql_info{1}, sql_info{2},'com.mysql.jdbc.Driver', url);
 
     end
     
