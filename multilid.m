02f221e75ca5d77795a6eb8544a5238efe76dfb0
saur1n <dr.saurin.parikh@gmail.com>
2020-10-23 12:29:50 -0400

    minor tweaks

--- multilid.m
+++ multilid.m
@@ -36,7 +36,7 @@
             lac = 'Y';
         else
             IL = 0;
-            lac = 'Y';
+            lac = 'N';
         end
         
         for e = expts
@@ -114,6 +114,15 @@
                     exec(conn, sprintf(['update %s ',...
                         'set average = NULL ',...
                         'where average <= 10'],tablename_lac));
+                else
+                    exec(conn, sprintf(['update %s ',...
+                        'set average = NULL ',...
+                        'where pos in ',...
+                        '(select pos from %s)'],tablename_clean,tablename_bpos));
+
+                    exec(conn, sprintf(['update %s ',...
+                        'set average = NULL ',...
+                        'where average <= 10'],tablename_clean));
                 end
 
                 %%  SPATIAL BIAS CORRECTION
@@ -255,142 +264,142 @@
                     sqlwrite(conn,tablename_pval,struct2table(pdata{iii}));
                 end
                 
-                %%  MCAT NORMALIZATION
-                
-                mcat_res = mcatNorm(hours(hours>10),density,n_plates,p2c_info,tablename_raw,sql_info);
-                
-                exec(conn, sprintf('drop table %s',tablename_mcat_norm));
-                exec(conn, sprintf(['create table %s ( ',...
-                            'pos int(11) not NULL, ',...
-                            'hours double not NULL, ',...
-                            'bg double default NULL, ',...
-                            'average double default NULL, ',...
-                            'fitness double default NULL, ',...
-                            'primary key (pos, hours))'],tablename_mcat_norm));
-                for i=1:length(hours(hours>10))
-                    datainsert(conn, tablename_mcat_norm,...
-                        {'pos','hours','bg','average','fitness'},mcat_res{i});
-                end
-
-                exec(conn, sprintf('drop table %s',tablename_mcat_fit)); 
-                exec(conn, sprintf(['create table %s (primary key (pos, hours))',...
-                    '(select c.strain_id, b.orf_name, a.pos, a.hours, a.bg, a.average, a.fitness ',...
-                    'from %s a, %s b , %s c ',...
-                    'where a.pos = b.pos and b.pos = c.pos ',...
-                    'order by a.hours, a.pos asc)'],...
-                    tablename_mcat_fit,tablename_mcat_norm,tablename_p2o,tablename_p2s));
-                
-                exec(conn, sprintf(['update %s ',...
-                    'set average = NULL, fitness = NULL ',...
-                    'where pos in ',...
-                    '(select pos from %s)'],tablename_mcat_fit,tablename_bpos));
-
-                exec(conn, sprintf(['update %s ',...
-                    'set average = NULL, fitness = NULL ',...
-                    'where average <= 10'],tablename_mcat_fit));
-                
-                %%  MCAT FITNESS STATS
-                
-                exec(conn, sprintf('drop table %s', tablename_mcat_fits));
-                exec(conn, sprintf(['create table %s (strain_id int not null, ',...
-                    'orf_name varchar(255) null, ',...
-                    'hours double not null, N int not null, cs_mean double null, ',...
-                    'cs_median double null, cs_std double null, ',...
-                    'primary key (strain_id, hours))'],tablename_mcat_fits));
-
-                colnames_fits = {'strain_id','orf_name','hours','N','cs_mean','cs_median','cs_std'};
-
-                mcat_stat_data = fitstats_sid(tablename_mcat_fit,sql_info);
-
-                tic
-                datainsert(conn,tablename_mcat_fits,colnames_fits,mcat_stat_data)
-                %     sqlwrite(conn,tablename_fits,struct2table(stat_data));
-                toc  
-
-                %%  MCAT P-VALUES
-                
-                hours = fetch(conn, sprintf(['select distinct hours from %s ',...
-                        'order by hours asc'], tablename_mcat_fits));
-                hours = hours.hours;
-                
-                exec(conn, sprintf('drop table %s',tablename_mcat_pval));
-                exec(conn, sprintf(['create table %s (strain_id int not null, ',...
-                    'orf_name varchar(255) null,'...
-                    'hours double not null, p double null, stat double null, ',...
-                    'es double null, ',...
-                    'primary key (strain_id, hours))'],tablename_mcat_pval));
-                colnames_pval = {'strain_id','orf_name','hours','p','stat','es'};
-
-                contpos = fetch(conn, sprintf(['select a.pos, a.rep_pos ',...
-                    'from %s a, %s b ',...
-                    'where a.density = %d and a.pos = b.pos and b.orf_name = "%s" ',...
-                    'and a.pos not in (select pos from %s) ',...
-                    'order by a.pos'],...
-                    tablename_p2p, tablename_p2o,...
-                    density, cont.name,...
-                    tablename_bpos));
-
-                iden_contpos = unique(contpos.pos);
-
-                for iii = 1:length(hours)
-                    contfit = [];
-                    for ii = 1:length(iden_contpos)
-                        cp = sprintf('%d,',contpos.rep_pos(contpos.pos == iden_contpos(ii)));
-                        temp = fetch(conn, sprintf(['select fitness from %s ',...
-                            'where hours = %0.2f and pos in (%s) ',...
-                            'and fitness is not null'],tablename_mcat_fit,hours(iii),...
-                            cp(1:end-1)));
-
-                        if nansum(temp.fitness) > 0
-                            outlier = isoutlier(temp.fitness);
-                            temp.fitness(outlier) = NaN;
-                            contfit = [contfit, nanmean(temp.fitness)];
-                        end
-                    end
-                    contmean = nanmean(contfit);
-                    contstd = nanstd(contfit);
-
-                    orffit = fetch(conn, sprintf(['select strain_id, ',...
-                        'orf_name, cs_median, ',...
-                        'cs_mean, cs_std from %s ',...
-                        'where hours = %0.2f and orf_name != ''%s'' ',...
-                        'order by orf_name asc'],tablename_mcat_fits,hours(iii),cont.name));
-
-                    m = contfit';
-                    tt = length(m);
-                    pvals = [];
-                    es = [];
-                    stat = [];
-                    for i = 1:length(orffit.strain_id)
-                        if sum(m<orffit.cs_mean(i)) < tt/2
-                            if m<orffit.cs_mean(i) == 0
-                                pvals = [pvals; 1/tt];
-                                es = [es; (orffit.cs_mean(i) - contmean)/contmean];
-                                stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
-                            else
-                                pvals = [pvals; ((sum(m<=orffit.cs_mean(i)))/tt)*2];
-                                es = [es; (orffit.cs_mean(i) - contmean)/contmean];
-                                stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
-                            end
-                        else
-                            pvals = [pvals; ((sum(m>=orffit.cs_mean(i)))/tt)*2];
-                            es = [es; (orffit.cs_mean(i) - contmean)/contmean];
-                            stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
-                        end
-                    end
-
-                    pdata{iii}.strain_id                                = orffit.strain_id;
-                    pdata{iii}.orf_name                                 = orffit.orf_name;
-                    pdata{iii}.hours                                    = ones(length(pdata{iii}.orf_name),1)*hours(iii);
-                    pdata{iii}.p                                        = num2cell(pvals);
-                    pdata{iii}.p(cellfun(@isnan,pdata{iii}.p))          = {[]};
-                    pdata{iii}.stat                                     = num2cell(stat);
-                    pdata{iii}.stat(cellfun(@isnan,pdata{iii}.stat))    = {[]};
-                    pdata{iii}.es                                       = num2cell(es);
-                    pdata{iii}.es(cellfun(@isnan,pdata{iii}.es))        = {[]};
-
-                    sqlwrite(conn,tablename_mcat_pval,struct2table(pdata{iii}));
-                end
+%                 %%  MCAT NORMALIZATION
+%                 
+%                 mcat_res = mcatNorm(hours(hours>10),density,n_plates,p2c_info,tablename_raw,sql_info);
+%                 
+%                 exec(conn, sprintf('drop table %s',tablename_mcat_norm));
+%                 exec(conn, sprintf(['create table %s ( ',...
+%                             'pos int(11) not NULL, ',...
+%                             'hours double not NULL, ',...
+%                             'bg double default NULL, ',...
+%                             'average double default NULL, ',...
+%                             'fitness double default NULL, ',...
+%                             'primary key (pos, hours))'],tablename_mcat_norm));
+%                 for i=1:length(hours(hours>10))
+%                     datainsert(conn, tablename_mcat_norm,...
+%                         {'pos','hours','bg','average','fitness'},mcat_res{i});
+%                 end
+% 
+%                 exec(conn, sprintf('drop table %s',tablename_mcat_fit)); 
+%                 exec(conn, sprintf(['create table %s (primary key (pos, hours))',...
+%                     '(select c.strain_id, b.orf_name, a.pos, a.hours, a.bg, a.average, a.fitness ',...
+%                     'from %s a, %s b , %s c ',...
+%                     'where a.pos = b.pos and b.pos = c.pos ',...
+%                     'order by a.hours, a.pos asc)'],...
+%                     tablename_mcat_fit,tablename_mcat_norm,tablename_p2o,tablename_p2s));
+%                 
+%                 exec(conn, sprintf(['update %s ',...
+%                     'set average = NULL, fitness = NULL ',...
+%                     'where pos in ',...
+%                     '(select pos from %s)'],tablename_mcat_fit,tablename_bpos));
+% 
+%                 exec(conn, sprintf(['update %s ',...
+%                     'set average = NULL, fitness = NULL ',...
+%                     'where average <= 10'],tablename_mcat_fit));
+%                 
+%                 %%  MCAT FITNESS STATS
+%                 
+%                 exec(conn, sprintf('drop table %s', tablename_mcat_fits));
+%                 exec(conn, sprintf(['create table %s (strain_id int not null, ',...
+%                     'orf_name varchar(255) null, ',...
+%                     'hours double not null, N int not null, cs_mean double null, ',...
+%                     'cs_median double null, cs_std double null, ',...
+%                     'primary key (strain_id, hours))'],tablename_mcat_fits));
+% 
+%                 colnames_fits = {'strain_id','orf_name','hours','N','cs_mean','cs_median','cs_std'};
+% 
+%                 mcat_stat_data = fitstats_sid(tablename_mcat_fit,sql_info);
+% 
+%                 tic
+%                 datainsert(conn,tablename_mcat_fits,colnames_fits,mcat_stat_data)
+%                 %     sqlwrite(conn,tablename_fits,struct2table(stat_data));
+%                 toc  
+% 
+%                 %%  MCAT P-VALUES
+%                 
+%                 hours = fetch(conn, sprintf(['select distinct hours from %s ',...
+%                         'order by hours asc'], tablename_mcat_fits));
+%                 hours = hours.hours;
+%                 
+%                 exec(conn, sprintf('drop table %s',tablename_mcat_pval));
+%                 exec(conn, sprintf(['create table %s (strain_id int not null, ',...
+%                     'orf_name varchar(255) null,'...
+%                     'hours double not null, p double null, stat double null, ',...
+%                     'es double null, ',...
+%                     'primary key (strain_id, hours))'],tablename_mcat_pval));
+%                 colnames_pval = {'strain_id','orf_name','hours','p','stat','es'};
+% 
+%                 contpos = fetch(conn, sprintf(['select a.pos, a.rep_pos ',...
+%                     'from %s a, %s b ',...
+%                     'where a.density = %d and a.pos = b.pos and b.orf_name = "%s" ',...
+%                     'and a.pos not in (select pos from %s) ',...
+%                     'order by a.pos'],...
+%                     tablename_p2p, tablename_p2o,...
+%                     density, cont.name,...
+%                     tablename_bpos));
+% 
+%                 iden_contpos = unique(contpos.pos);
+% 
+%                 for iii = 1:length(hours)
+%                     contfit = [];
+%                     for ii = 1:length(iden_contpos)
+%                         cp = sprintf('%d,',contpos.rep_pos(contpos.pos == iden_contpos(ii)));
+%                         temp = fetch(conn, sprintf(['select fitness from %s ',...
+%                             'where hours = %0.2f and pos in (%s) ',...
+%                             'and fitness is not null'],tablename_mcat_fit,hours(iii),...
+%                             cp(1:end-1)));
+% 
+%                         if nansum(temp.fitness) > 0
+%                             outlier = isoutlier(temp.fitness);
+%                             temp.fitness(outlier) = NaN;
+%                             contfit = [contfit, nanmean(temp.fitness)];
+%                         end
+%                     end
+%                     contmean = nanmean(contfit);
+%                     contstd = nanstd(contfit);
+% 
+%                     orffit = fetch(conn, sprintf(['select strain_id, ',...
+%                         'orf_name, cs_median, ',...
+%                         'cs_mean, cs_std from %s ',...
+%                         'where hours = %0.2f and orf_name != ''%s'' ',...
+%                         'order by orf_name asc'],tablename_mcat_fits,hours(iii),cont.name));
+% 
+%                     m = contfit';
+%                     tt = length(m);
+%                     pvals = [];
+%                     es = [];
+%                     stat = [];
+%                     for i = 1:length(orffit.strain_id)
+%                         if sum(m<orffit.cs_mean(i)) < tt/2
+%                             if m<orffit.cs_mean(i) == 0
+%                                 pvals = [pvals; 1/tt];
+%                                 es = [es; (orffit.cs_mean(i) - contmean)/contmean];
+%                                 stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
+%                             else
+%                                 pvals = [pvals; ((sum(m<=orffit.cs_mean(i)))/tt)*2];
+%                                 es = [es; (orffit.cs_mean(i) - contmean)/contmean];
+%                                 stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
+%                             end
+%                         else
+%                             pvals = [pvals; ((sum(m>=orffit.cs_mean(i)))/tt)*2];
+%                             es = [es; (orffit.cs_mean(i) - contmean)/contmean];
+%                             stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
+%                         end
+%                     end
+% 
+%                     pdata{iii}.strain_id                                = orffit.strain_id;
+%                     pdata{iii}.orf_name                                 = orffit.orf_name;
+%                     pdata{iii}.hours                                    = ones(length(pdata{iii}.orf_name),1)*hours(iii);
+%                     pdata{iii}.p                                        = num2cell(pvals);
+%                     pdata{iii}.p(cellfun(@isnan,pdata{iii}.p))          = {[]};
+%                     pdata{iii}.stat                                     = num2cell(stat);
+%                     pdata{iii}.stat(cellfun(@isnan,pdata{iii}.stat))    = {[]};
+%                     pdata{iii}.es                                       = num2cell(es);
+%                     pdata{iii}.es(cellfun(@isnan,pdata{iii}.es))        = {[]};
+% 
+%                     sqlwrite(conn,tablename_mcat_pval,struct2table(pdata{iii}));
+%                 end
                 
             end
             fprintf('Done with %s_%d.\n', expt_set, d)
