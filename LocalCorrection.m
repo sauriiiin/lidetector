2260380d231724a831fa8dc94ba3212e6bdf2ab2
saurin parikh <dr.saurin.parikh@gmail.com>
2022-02-07 08:13:30 -0500

    minor updates

--- LocalCorrection.m
+++ LocalCorrection.m
@@ -9,54 +9,62 @@
 %%
 
 function out = LocalCorrection(p2c_info,density,cont_name,...
-        tablename_s2o,tablename_p2s,tablename_raw,sql_info)
+        tablename_s2o,tablename_p2s,tablename_raw,sql_info,...
+        toolkit_path,expt)
 
     conn = connSQL(sql_info);
 
     cont.id = fetch(conn, sprintf(['select strain_id from %s ',...
         'where orf_name = "%s" '], tablename_s2o, cont_name));
     cont.id = cont.id.strain_id;
-
-    p2c = fetch(conn, sprintf(['select * from %s a ',...
-            'where density = %d ',...
-            'order by a.%s, a.%s, a.%s'],...
-            p2c_info{1},density,p2c_info{2},p2c_info{4},p2c_info{3}));
-
-    pos = [];
-    neigh = zeros(density*length(unique(p2c.plate_no)),8);
-    neigh_sr = zeros(density*length(unique(p2c.plate_no)),8);
-    i = 1;
-    for pl = unique(p2c.plate_no)'
-        temp = p2c(p2c.plate_no == pl,:);
-        for c = sort(unique(temp.plate_col))'
-            for r = sort(unique(temp.plate_row(temp.plate_col == c)))'
-                pos = [pos; temp.pos(temp.plate_col == c & temp.plate_row == r)];
-
-                neigh(i,1) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r - 1));
-                neigh(i,2) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r));
-                neigh(i,3) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r + 1));
-                neigh(i,4) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r - 1));
-                neigh(i,5) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r + 1));
-                neigh(i,6) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r - 1));
-                neigh(i,7) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r));
-                neigh(i,8) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r + 1));
-                
-                if density >= 6144
-                    neigh_sr(i,1) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r - 2));
-                    neigh_sr(i,2) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r));
-                    neigh_sr(i,3) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r + 2));
-                    neigh_sr(i,4) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r - 2));
-                    neigh_sr(i,5) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r + 2));
-                    neigh_sr(i,6) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r - 2));
-                    neigh_sr(i,7) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r));
-                    neigh_sr(i,8) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r + 2));
-                end
-                i = i + 1;
-            end
-        end
+    
+    if exist(sprintf('%s/%s_GRIDS.mat',toolkit_path,expt), 'file') == 1
+        load(sprintf('%s/%s_GRIDS.mat',toolkit_path,expt), 'grids', 'grids_sr');
+    else
+        [grids, grids_sr] = neighbor_pos(p2c_info,density,sql_info,...
+            toolkit_path,expt);
     end
-    grids = [pos, neigh];
-    grids_sr = [pos, neigh_sr];
+
+%     p2c = fetch(conn, sprintf(['select * from %s a ',...
+%             'where density = %d ',...
+%             'order by a.%s, a.%s, a.%s'],...
+%             p2c_info{1},density,p2c_info{2},p2c_info{4},p2c_info{3}));
+% 
+%     pos = [];
+%     neigh = zeros(density*length(unique(p2c.plate_no)),8);
+%     neigh_sr = zeros(density*length(unique(p2c.plate_no)),8);
+%     i = 1;
+%     for pl = unique(p2c.plate_no)'
+%         temp = p2c(p2c.plate_no == pl,:);
+%         for c = sort(unique(temp.plate_col))'
+%             for r = sort(unique(temp.plate_row(temp.plate_col == c)))'
+%                 pos = [pos; temp.pos(temp.plate_col == c & temp.plate_row == r)];
+% 
+%                 neigh(i,1) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r - 1));
+%                 neigh(i,2) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r));
+%                 neigh(i,3) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r + 1));
+%                 neigh(i,4) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r - 1));
+%                 neigh(i,5) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r + 1));
+%                 neigh(i,6) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r - 1));
+%                 neigh(i,7) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r));
+%                 neigh(i,8) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r + 1));
+%                 
+%                 if density >= 6144
+%                     neigh_sr(i,1) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r - 2));
+%                     neigh_sr(i,2) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r));
+%                     neigh_sr(i,3) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r + 2));
+%                     neigh_sr(i,4) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r - 2));
+%                     neigh_sr(i,5) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r + 2));
+%                     neigh_sr(i,6) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r - 2));
+%                     neigh_sr(i,7) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r));
+%                     neigh_sr(i,8) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r + 2));
+%                 end
+%                 i = i + 1;
+%             end
+%         end
+%     end
+%     grids = [pos, neigh];
+%     grids_sr = [pos, neigh_sr];
 
     rawdata = fetch(conn, sprintf(['select b.*, c.strain_id, a.hours, a.average ',...
         'from %s a, %s b, %s c ',...
