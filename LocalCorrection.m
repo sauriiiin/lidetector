b5facdd3b76b1126620d10223430c4111c6c4cfe
saur1n <dr.saurin.parikh@gmail.com>
2022-02-07 07:46:16 -0500

    minor updates with new mysql syntax

--- LocalCorrection.m
+++ LocalCorrection.m
@@ -23,33 +23,33 @@ function out = LocalCorrection(p2c_info,density,cont_name,...
             p2c_info{1},density,p2c_info{2},p2c_info{4},p2c_info{3}));
 
     pos = [];
-    neigh = zeros(density*length(unique(p2c.plate)),8);
-    neigh_sr = zeros(density*length(unique(p2c.plate)),8);
+    neigh = zeros(density*length(unique(p2c.plate_no)),8);
+    neigh_sr = zeros(density*length(unique(p2c.plate_no)),8);
     i = 1;
-    for pl = unique(p2c.plate)'
-        temp = p2c(p2c.plate == pl,:);
-        for c = sort(unique(temp.col))'
-            for r = sort(unique(temp.row(temp.col == c)))'
-                pos = [pos; temp.pos(temp.col == c & temp.row == r)];
+    for pl = unique(p2c.plate_no)'
+        temp = p2c(p2c.plate_no == pl,:);
+        for c = sort(unique(temp.plate_col))'
+            for r = sort(unique(temp.plate_row(temp.plate_col == c)))'
+                pos = [pos; temp.pos(temp.plate_col == c & temp.plate_row == r)];
 
-                neigh(i,1) = isnotempty(temp.pos(temp.col == c - 1 & temp.row == r - 1));
-                neigh(i,2) = isnotempty(temp.pos(temp.col == c - 1 & temp.row == r));
-                neigh(i,3) = isnotempty(temp.pos(temp.col == c - 1 & temp.row == r + 1));
-                neigh(i,4) = isnotempty(temp.pos(temp.col == c & temp.row == r - 1));
-                neigh(i,5) = isnotempty(temp.pos(temp.col == c & temp.row == r + 1));
-                neigh(i,6) = isnotempty(temp.pos(temp.col == c + 1 & temp.row == r - 1));
-                neigh(i,7) = isnotempty(temp.pos(temp.col == c + 1 & temp.row == r));
-                neigh(i,8) = isnotempty(temp.pos(temp.col == c + 1 & temp.row == r + 1));
+                neigh(i,1) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r - 1));
+                neigh(i,2) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r));
+                neigh(i,3) = isnotempty(temp.pos(temp.plate_col == c - 1 & temp.plate_row == r + 1));
+                neigh(i,4) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r - 1));
+                neigh(i,5) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r + 1));
+                neigh(i,6) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r - 1));
+                neigh(i,7) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r));
+                neigh(i,8) = isnotempty(temp.pos(temp.plate_col == c + 1 & temp.plate_row == r + 1));
                 
                 if density >= 6144
-                    neigh_sr(i,1) = isnotempty(temp.pos(temp.col == c - 2 & temp.row == r - 2));
-                    neigh_sr(i,2) = isnotempty(temp.pos(temp.col == c - 2 & temp.row == r));
-                    neigh_sr(i,3) = isnotempty(temp.pos(temp.col == c - 2 & temp.row == r + 2));
-                    neigh_sr(i,4) = isnotempty(temp.pos(temp.col == c & temp.row == r - 2));
-                    neigh_sr(i,5) = isnotempty(temp.pos(temp.col == c & temp.row == r + 2));
-                    neigh_sr(i,6) = isnotempty(temp.pos(temp.col == c + 2 & temp.row == r - 2));
-                    neigh_sr(i,7) = isnotempty(temp.pos(temp.col == c + 2 & temp.row == r));
-                    neigh_sr(i,8) = isnotempty(temp.pos(temp.col == c + 2 & temp.row == r + 2));
+                    neigh_sr(i,1) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r - 2));
+                    neigh_sr(i,2) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r));
+                    neigh_sr(i,3) = isnotempty(temp.pos(temp.plate_col == c - 2 & temp.plate_row == r + 2));
+                    neigh_sr(i,4) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r - 2));
+                    neigh_sr(i,5) = isnotempty(temp.pos(temp.plate_col == c & temp.plate_row == r + 2));
+                    neigh_sr(i,6) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r - 2));
+                    neigh_sr(i,7) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r));
+                    neigh_sr(i,8) = isnotempty(temp.pos(temp.plate_col == c + 2 & temp.plate_row == r + 2));
                 end
                 i = i + 1;
             end
@@ -65,13 +65,13 @@ function out = LocalCorrection(p2c_info,density,cont_name,...
         tablename_raw,...
         p2c_info{1},tablename_p2s,...
         p2c_info{2},p2c_info{4},p2c_info{3}));
-    rawdata.colony(rawdata.strain_id == cont.id) = 1;
+    rawdata.plate_colony(rawdata.strain_id == cont.id) = 1;
 
     %% COMPETITION CORRECTION
     out = [];
     for hr = sort(unique(rawdata.hours))'
-        for pl = sort(unique(rawdata.plate(rawdata.hours == hr)))'
-            tempdat = rawdata(rawdata.hours == hr & rawdata.plate == pl,:);
+        for pl = sort(unique(rawdata.plate_no(rawdata.hours == hr)))'
+            tempdat = rawdata(rawdata.hours == hr & rawdata.plate_no == pl,:);
 %             tempdat.average(tempdat.strain_id == 0) = 0;
 
             for i = 1:size(grids, 1)
